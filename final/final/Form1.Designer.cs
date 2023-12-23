94f697b2b2e607b64883ce66573903f4e4125a48
DalGbr <92860732+DalGbr@users.noreply.github.com>
2023-12-23 11:35:13 +0200

    Add files via upload

---
+++ final/final/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace final
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            textBox3 = new TextBox();
+            button5 = new Button();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(45, 237);
+            button1.Margin = new Padding(2, 3, 2, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(151, 48);
+            button1.TabIndex = 1;
+            button1.Text = "Generate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(170, 152);
+            textBox2.Margin = new Padding(2, 3, 2, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(218, 41);
+            textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(31, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 35);
+            label1.TabIndex = 7;
+            label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(31, 158);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 35);
+            label2.TabIndex = 8;
+            label2.Text = "Password:";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(45, 291);
+            button2.Margin = new Padding(2, 3, 2, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(151, 48);
+            button2.TabIndex = 9;
+            button2.Text = "Write";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(200, 70);
+            textBox1.Margin = new Padding(2, 3, 2, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(188, 41);
+            textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(231, 237);
+            button3.Margin = new Padding(2, 3, 2, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(157, 48);
+            button3.TabIndex = 10;
+            button3.Text = "File";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(231, 291);
+            button4.Margin = new Padding(2, 3, 2, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(157, 48);
+            button4.TabIndex = 11;
+            button4.Text = "Exit";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(468, 73);
+            textBox3.Margin = new Padding(2, 3, 2, 3);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Put here encrypted password";
+            textBox3.Size = new Size(336, 41);
+            textBox3.TabIndex = 12;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(511, 208);
+            button5.Margin = new Padding(2, 3, 2, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(250, 48);
+            button5.TabIndex = 13;
+            button5.Text = "Decrypt";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(468, 132);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Decryption output";
+            textBox4.Size = new Size(336, 41);
+            textBox4.TabIndex = 14;
+            // 
+            // label3
+            // 
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Location = new Point(423, 30);
+            label3.Name = "label3";
+            label3.Size = new Size(430, 263);
+            label3.TabIndex = 15;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(571, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(137, 35);
+            label4.TabIndex = 16;
+            label4.Text = "Decryption";
+            // 
+            // label5
+            // 
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.Location = new Point(12, 30);
+            label5.Name = "label5";
+            label5.Size = new Size(406, 338);
+            label5.TabIndex = 17;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(98, 9);
+            label6.Name = "label6";
+            label6.Size = new Size(238, 35);
+            label6.TabIndex = 18;
+            label6.Text = "Password Generator";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(865, 396);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(button5);
+            Controls.Add(textBox3);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label5);
+            Margin = new Padding(2, 3, 2, 3);
+            Name = "Form1";
+            Text = "PassGen";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button button1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button button2;
+        private TextBox textBox1;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox3;
+        private Button button5;
+        private TextBox textBox4;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ final/final/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System.Text;
+using System.Diagnostics;
+
+namespace final
+{
+
+    public partial class Form1 : Form
+    {
+
+        Password password = new Password();
+        string file = "passwords.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = password.GeneratePassword();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Password password1 = new Password();
+            string username = textBox1.Text;
+            string password = password1.EncryptPassword(textBox2.Text, "7N8tK7y4p8zR1/3f6+9V1g==\r\n");
+
+
+            using (StreamWriter outputFile = new StreamWriter(file, true))
+            {
+                outputFile.WriteLine(username + ":" + password);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", file);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Password password2 = new Password();
+            string password = password2.DecryptPassword(textBox3.Text, "7N8tK7y4p8zR1/3f6+9V1g==\r\n");
+            textBox4.Text = password;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+
+    public class Password
+    {
+        int passSize;
+
+        public Password()
+        {
+            Random rnd = new Random();
+            passSize = rnd.Next(8, 14);
+        }
+
+        public string GeneratePassword()
+        {
+            string lower = "abcdefghijklmnopqrstuvwxyz";
+            string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string digits = "0123456789";
+            string punctuation = "!@#$%^&*_-+=:;,.?/|~";
+
+            StringBuilder password = new StringBuilder();
+            Random random = new Random();
+
+            password.Append(lower[random.Next(lower.Length)]);
+            password.Append(upper[random.Next(upper.Length)]);
+            password.Append(digits[random.Next(digits.Length)]);
+            password.Append(punctuation[random.Next(punctuation.Length)]);
+
+            for (int i = passSize - 4; i >= 0; i--)
+            {
+                string ratio = lower + upper + digits + punctuation;
+                int index = random.Next(ratio.Length);
+                password.Append(ratio[index]);
+            }
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                int swap = random.Next(password.Length);
+                char temp = password[i];
+                password[i] = password[swap];
+                password[swap] = temp;
+            }
+
+            return password.ToString();
+        }
+
+        public string EncryptPassword(string password, string secretKey)
+        {
+            StringBuilder encryptedPassword = new StringBuilder();
+            for (int i = 0; i < password.Length; i++)
+            {
+                encryptedPassword.Append((char)(password[i] ^ secretKey[i % secretKey.Length]));
+            }
+            return encryptedPassword.ToString();
+        }
+
+        public string DecryptPassword(string encryptedPassword, string secretKey)
+        {
+            StringBuilder decryptedPassword = new StringBuilder();
+            for (int i = 0; i < encryptedPassword.Length; i++)
+            {
+                decryptedPassword.Append((char)(encryptedPassword[i] ^ secretKey[i % secretKey.Length]));
+            }
+            return decryptedPassword.ToString();
+        }
+
+
+    }
+
+}
+
+
+
